8fcb6eb8fe78bfe29460bc78d45c38fc12c137c6
Колдун Ебучий <andrey11551611@gmail.com>
2022-05-24 22:28:19 +0300

    box detector

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 from PIL import Image, ImageChops, ImageEnhance
-im = Image.open("f.png")
+import itertools
+im = Image.open("f.jpg")
 img = im.convert("RGB")
 pixdata = img.load()
+nums=[]
+gren=[]
+found=False
+innum=0
 for x in range(img.size[0]):
+    if innum >17:
+        right=x-1
+        found = False
+        nums.append([left,right])
+    innum=0
+    for y in range(2,20):
+        if pixdata[x, y]!=(255, 255, 255):
+            if found is False:
+                left=x
+                found=True
+        else:
+            if found is True:
+                innum+=1
+found=False
+for x in reversed(range(img.size[0])):
+    if found is  True:
+        break
     for y in range(5,16):
-        if pixdata[x, 11]!=(255, 255, 255):
-            left=x
-            break
-print(x)
+        if pixdata[x, y]!=(255, 255, 255):
+            right=x
+            found=True
+print(left,right)
+print(nums)
+for i in nums:
+    x, y = i
+    if y-x<6:
+        gren.append(i)
+for i in gren:
+    nums.remove(i)
+for i in nums:
+    x,y=i
+    for l in range(x,y):
+        pixdata[l, 11] = (255, 53, 53)
+# dump=img.crop((left, 0, right, img.size[1])).save('dump.jpg', quality=100)
+print(nums)
+img.show()
+
